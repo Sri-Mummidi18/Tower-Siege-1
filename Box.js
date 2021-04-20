1ff2557a0a40421c802859bcdb60f557c997b943
Sri Mummidi <69703202+Sri-Mummidi18@users.noreply.github.com>
2021-04-20 16:24:52 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,55 @@
+class Box extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill(130, 237, 237);
+    super.display();
+  }
+}
+
+class Box1  
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill(230, 190, 234)
+    super.display();
+  }
+}
+
+class Box2 extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill(60, 242, 163);
+    super.display();
+  }
+}
+
+class Box3 extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill(239,242,72);
+    super.display();
+  }
+}
